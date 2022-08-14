56dadd7f997f4bb1823040c206552398b89105b5
SkMishra77 <sanatkumarmishra02@gmail.com>
2022-08-14 13:03:07 +0530

    scroll opacity down

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.opacity = 1;
+    
   } 
   else if(currentScrollPos>140)
   {
     var abc=(140-currentScrollPos)/3;
+    var opa=Math.abs(abc);
+    opa=1/opa;
     var abcd=abc+"px";
     document.getElementById("navbar").style.top = abcd;
-        
+    console.log(abcd)
+    document.getElementById("navbar").style.opacity = opa;
   }  
   prevScrollpos = currentScrollPos;
 }
