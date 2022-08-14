5b72373b853b947b954e46e126edabd916cd0ff5
SkMishra77 <sanatkumarmishra02@gmail.com>
2022-08-14 12:32:55 +0530

    scroll animation @140px

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 var prevScrollpos = window.pageYOffset;
-
+var animat =document.getElementById("navbar");
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } 
-  else 
+  else if(currentScrollPos>140)
   {
-    document.getElementById("navbar").style.top = "-100px";
-  }
+    var abc=(140-currentScrollPos)/3;
+    var abcd=abc+"px";
+    document.getElementById("navbar").style.top = abcd;
+        
+  }  
   prevScrollpos = currentScrollPos;
 }
