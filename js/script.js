ad6053907edb941827edf26d0d490eca8f0ab0eb
deadland2002 <satvikshukla453@gmail.com>
2022-08-25 13:20:22 +0530

    falcon-9
    hi

--- js/script.js
+++ js/script.js
@@ -59,3 +59,51 @@ function navToggle()
   document.body.classList.toggle("stop-scrolling");
   Xmenu.classList.toggle("show-menu");
 }
+
+
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+document.addEventListener('scroll', scrollPage);
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute('data-target');
+      // Get current counter value
+      const c = +counter.innerText;
+
+      // Create an increment
+      const increment = target / 50;
+
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = '0'));
+}
